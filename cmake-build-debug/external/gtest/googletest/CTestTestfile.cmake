8e1fa865fa24e82abbd4ab5a8122be62ce41f0a0
ICWeiner <diogoffn@gmail.com>
2022-01-05 09:07:19 -0800

    first commit

---
+++ cmake-build-debug/external/gtest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/diogo/Documents/svg/external/gtest/googletest
+# Build directory: /home/diogo/Documents/svg/cmake-build-debug/external/gtest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
